4ce6fd9bea17722a1380a2af8cfe9e9a53b308dc
quotexpro882-source <quotexpro882@gmail.com>
2026-03-09 12:37:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def handle_forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
     f"💎 𝗣𝗿𝗲𝗺𝗶𝘂𝗺 𝗦𝗶𝗴𝗻𝗮𝗹 💎\n"
     f"━━━━━━━━━━━━━━━\n"
     f"👇 <u><b>OFFICIAL REGISTRATION LINK</b></u>\n"
-    f"👉 <a href='https://broker-qx.pro/sign-up/?lid=1200739'>Join Free VIP REGISTER HERE</a>\n\n"
+    f"👉 <a href='https://broker-qx.pro/sign-up/?lid=398722'>Join Free VIP REGISTER HERE</a>\n\n"
     f"🎁 <u><b>USE CODE:</b></u> <code>Masterguru</code>\n"
     f"💥 <u><b>GET INSTANT 50% BONUS ON FIRST DEPOSIT!</b></u>\n"
     f"<i>(Valid only via this official link)</i>\n"
@@ -237,6 +237,7 @@ if __name__ == "__main__":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
     asyncio.run(main())
+
 
 
 
